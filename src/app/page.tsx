67d0de5cc893303b5da1437b90362056b20db6cf
iniadittt <aditya.braincore@gmail.com>
2024-12-03 15:29:37 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Link from "next/link";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-
   return (
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -36,9 +34,7 @@ export default async function Home() {
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello ? hello.greeting : "Loading tRPC query..."}
-            </p>
+            <p className="text-2xl text-white">NEXTJS TRPC</p>
           </div>
           <Link href={"/sign-in"}>Login</Link>
         </div>
