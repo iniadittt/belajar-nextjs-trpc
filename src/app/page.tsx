be3931737938de9165de4a3f4de23969e66f1a27
iniadittt <aditya.braincore@gmail.com>
2024-12-03 15:29:43 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
   return (
